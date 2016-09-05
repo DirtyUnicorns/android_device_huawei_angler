811694a2bdada3a6bd47a7407d5f96ace97ee355
beanstown106 <nbenis106@gmail.com>
2016-09-04 20:49:50 -0400

    Angler: Always build aosp_symlinks
    Change-Id: Ib949ecdddd3120296619fce36027f7d77eeca878

--- device.mk
+++ device.mk
@@ -300,6 +300,10 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+# Misc
+PRODUCT_PACKAGES += \
+    AOSPLinks
+
 # Bluetooth HAL
 PRODUCT_PACKAGES += \
     libbt-vendor \
