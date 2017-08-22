f7a96da959794dff4630aa9a808b1966a4a204d7
Josh Chasky <jchasky@gmail.com>
2017-08-22 23:43:29 +0200

    Angler: Add DU makefile
    Change-Id: I30302818a80533365a5f425ec33571342504014b

---
+++ du.mk
@@ -0,0 +1,41 @@
+#
+# Copyright 2013-2017 The Dirty Unicorns Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Sample: This is where we'd set a backup provider if we had one
+# $(call inherit-product, device/sample/products/backup_overlay.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, device/huawei/angler/aosp_angler.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
+
+PRODUCT_NAME := du_angler
+PRODUCT_DEVICE := angler
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 6P
+PRODUCT_MANUFACTURER := Huawei
+
+# DU updater prop
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.du.updater=angler
+
+# Device Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=angler \
+    BUILD_FINGERPRINT=google/angler/angler:7.1.2/N2G47H/3783593:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 7.1.2 N2G47H 3783593 release-keys"
