6ee5c29bec5b16c225c65bce3d6a51b49a8a92ad
Josh Chasky <jchasky@gmail.com>
2017-08-22 23:43:29 +0200

    Angler: Build kernel inline
    Change-Id: Idbd5065d75d428ae9595ff47eded56567d4539d8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,6 +41,11 @@ BOARD_KERNEL_CMDLINE += loop.max_part=7
 
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 
+TARGET_KERNEL_CONFIG := angler_defconfig
+TARGET_KERNEL_SOURCE := kernel/huawei/angler
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+
 BOARD_USES_ALSA_AUDIO := true
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
