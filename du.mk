729251a926b4dc850e5198b3a5f95f5282211cfb
Josh Chasky <jchasky@gmail.com>
2018-05-18 07:04:24 -0400

    Update build fingerprint to OPM2.171019.029.A1
    Change-Id: I63774e4d8c4447beb50d01d5641328ed126f7a54

--- du.mk
+++ du.mk
@@ -37,5 +37,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:8.1.0/OPM3.171019.019/4618994:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 8.1.0 OPM3.171019.019 4618994 release-keys"
+    BUILD_FINGERPRINT=google/angler/angler:8.1.0/OPM2.171019.029.A1/4720889:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user OPM2.171019.029.A1 4720889 release-keys"
